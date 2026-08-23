ce38328f4495da481b410d3079c4c6814a6feb79
UUoocl <99063397+UUoocl@users.noreply.github.com>
2026-08-23 09:10:34 -0400

    feat: add Syphon integration support for macOS with SyphonIn, SyphonOutTexture, and SyphonOutPatchCanvas ops

---
+++ ops/Ops.Extension.Standalone.MacOs.Syphon.SyphonIn/build/Release/.deps/Release/obj.target/syphon_in_client/src/syphon_in_client.o.d
@@ -0,0 +1,44 @@
+cmd_Release/obj.target/syphon_in_client/src/syphon_in_client.o := c++ '-DNODE_GYP_MODULE_NAME=syphon_in_client' '-DUSING_UV_SHARED=1' '-DUSING_V8_SHARED=1' '-DV8_DEPRECATION_WARNINGS=1' '-D_GLIBCXX_USE_CXX11_ABI=1' '-D_FILE_OFFSET_BITS=64' '-D_DARWIN_USE_64_BIT_INODE=1' '-D_LARGEFILE_SOURCE' '-DOPENSSL_NO_PINSHARED' '-DOPENSSL_THREADS' '-DNAPI_DISABLE_CPP_EXCEPTIONS' '-DBUILDING_NODE_EXTENSION' -I/Users/jonwood/Library/Caches/node-gyp/24.18.1/include/node -I/Users/jonwood/Library/Caches/node-gyp/24.18.1/src -I/Users/jonwood/Library/Caches/node-gyp/24.18.1/deps/openssl/config -I/Users/jonwood/Library/Caches/node-gyp/24.18.1/deps/openssl/openssl/include -I/Users/jonwood/Library/Caches/node-gyp/24.18.1/deps/uv/include -I/Users/jonwood/Library/Caches/node-gyp/24.18.1/deps/zlib -I/Users/jonwood/Library/Caches/node-gyp/24.18.1/deps/v8/include -I/Users/jonwood/Github_local_dev/node_modules/node-addon-api -I/Users/jonwood/Github_local_dev/cablesStudio/ops/Ops.Extension.Standalone.MacOs.Syphon.SyphonIn/Frameworks/Syphon.framework/Headers -I/Users/jonwood/Github_local_dev/cablesStudio/ops/Ops.Extension.Standalone.MacOs.Syphon.SyphonIn/Frameworks/Syphon.framework/Versions/A/Headers  -O3 -gdwarf-2 -fno-strict-aliasing -mmacosx-version-min=13.0 -arch arm64 -Wall -Wendif-labels -W -Wno-unused-parameter -std=gnu++20 -stdlib=libc++ -fno-rtti -std=c++17 -stdlib=libc++ -ObjC++ -fobjc-arc -F/Users/jonwood/Github_local_dev/cablesStudio/ops/Ops.Extension.Standalone.MacOs.Syphon.SyphonIn/Frameworks -fobjc-arc -MMD -MF ./Release/.deps/Release/obj.target/syphon_in_client/src/syphon_in_client.o.d.raw -c -o Release/obj.target/syphon_in_client/src/syphon_in_client.o ../src/syphon_in_client.mm
+Release/obj.target/syphon_in_client/src/syphon_in_client.o: \
+  ../src/syphon_in_client.mm \
+  /Users/jonwood/Github_local_dev/node_modules/node-addon-api/napi.h \
+  /Users/jonwood/Library/Caches/node-gyp/24.18.1/include/node/node_api.h \
+  /Users/jonwood/Library/Caches/node-gyp/24.18.1/include/node/js_native_api.h \
+  /Users/jonwood/Library/Caches/node-gyp/24.18.1/include/node/js_native_api_types.h \
+  /Users/jonwood/Library/Caches/node-gyp/24.18.1/include/node/node_api_types.h \
+  /Users/jonwood/Github_local_dev/node_modules/node-addon-api/napi-inl.h \
+  /Users/jonwood/Github_local_dev/node_modules/node-addon-api/napi-inl.deprecated.h \
+  /Users/jonwood/Github_local_dev/cablesStudio/ops/Ops.Extension.Standalone.MacOs.Syphon.SyphonIn/Frameworks/Syphon.framework/Headers/Syphon.h \
+  /Users/jonwood/Github_local_dev/cablesStudio/ops/Ops.Extension.Standalone.MacOs.Syphon.SyphonIn/Frameworks/Syphon.framework/Headers/SyphonServerDirectory.h \
+  /Users/jonwood/Github_local_dev/cablesStudio/ops/Ops.Extension.Standalone.MacOs.Syphon.SyphonIn/Frameworks/Syphon.framework/Headers/SyphonMetalServer.h \
+  /Users/jonwood/Github_local_dev/cablesStudio/ops/Ops.Extension.Standalone.MacOs.Syphon.SyphonIn/Frameworks/Syphon.framework/Headers/SyphonServerBase.h \
+  /Users/jonwood/Github_local_dev/cablesStudio/ops/Ops.Extension.Standalone.MacOs.Syphon.SyphonIn/Frameworks/Syphon.framework/Headers/SyphonMetalClient.h \
+  /Users/jonwood/Github_local_dev/cablesStudio/ops/Ops.Extension.Standalone.MacOs.Syphon.SyphonIn/Frameworks/Syphon.framework/Headers/SyphonClientBase.h \
+  /Users/jonwood/Github_local_dev/cablesStudio/ops/Ops.Extension.Standalone.MacOs.Syphon.SyphonIn/Frameworks/Syphon.framework/Headers/SyphonOpenGLServer.h \
+  /Users/jonwood/Github_local_dev/cablesStudio/ops/Ops.Extension.Standalone.MacOs.Syphon.SyphonIn/Frameworks/Syphon.framework/Headers/SyphonOpenGLClient.h \
+  /Users/jonwood/Github_local_dev/cablesStudio/ops/Ops.Extension.Standalone.MacOs.Syphon.SyphonIn/Frameworks/Syphon.framework/Headers/SyphonOpenGLImage.h \
+  /Users/jonwood/Github_local_dev/cablesStudio/ops/Ops.Extension.Standalone.MacOs.Syphon.SyphonIn/Frameworks/Syphon.framework/Headers/SyphonImageBase.h \
+  /Users/jonwood/Github_local_dev/cablesStudio/ops/Ops.Extension.Standalone.MacOs.Syphon.SyphonIn/Frameworks/Syphon.framework/Headers/SyphonServer.h \
+  /Users/jonwood/Github_local_dev/cablesStudio/ops/Ops.Extension.Standalone.MacOs.Syphon.SyphonIn/Frameworks/Syphon.framework/Headers/SyphonClient.h \
+  /Users/jonwood/Github_local_dev/cablesStudio/ops/Ops.Extension.Standalone.MacOs.Syphon.SyphonIn/Frameworks/Syphon.framework/Headers/SyphonImage.h
+../src/syphon_in_client.mm:
+/Users/jonwood/Github_local_dev/node_modules/node-addon-api/napi.h:
+/Users/jonwood/Library/Caches/node-gyp/24.18.1/include/node/node_api.h:
+/Users/jonwood/Library/Caches/node-gyp/24.18.1/include/node/js_native_api.h:
+/Users/jonwood/Library/Caches/node-gyp/24.18.1/include/node/js_native_api_types.h:
+/Users/jonwood/Library/Caches/node-gyp/24.18.1/include/node/node_api_types.h:
+/Users/jonwood/Github_local_dev/node_modules/node-addon-api/napi-inl.h:
+/Users/jonwood/Github_local_dev/node_modules/node-addon-api/napi-inl.deprecated.h:
+/Users/jonwood/Github_local_dev/cablesStudio/ops/Ops.Extension.Standalone.MacOs.Syphon.SyphonIn/Frameworks/Syphon.framework/Headers/Syphon.h:
+/Users/jonwood/Github_local_dev/cablesStudio/ops/Ops.Extension.Standalone.MacOs.Syphon.SyphonIn/Frameworks/Syphon.framework/Headers/SyphonServerDirectory.h:
+/Users/jonwood/Github_local_dev/cablesStudio/ops/Ops.Extension.Standalone.MacOs.Syphon.SyphonIn/Frameworks/Syphon.framework/Headers/SyphonMetalServer.h:
+/Users/jonwood/Github_local_dev/cablesStudio/ops/Ops.Extension.Standalone.MacOs.Syphon.SyphonIn/Frameworks/Syphon.framework/Headers/SyphonServerBase.h:
+/Users/jonwood/Github_local_dev/cablesStudio/ops/Ops.Extension.Standalone.MacOs.Syphon.SyphonIn/Frameworks/Syphon.framework/Headers/SyphonMetalClient.h:
+/Users/jonwood/Github_local_dev/cablesStudio/ops/Ops.Extension.Standalone.MacOs.Syphon.SyphonIn/Frameworks/Syphon.framework/Headers/SyphonClientBase.h:
+/Users/jonwood/Github_local_dev/cablesStudio/ops/Ops.Extension.Standalone.MacOs.Syphon.SyphonIn/Frameworks/Syphon.framework/Headers/SyphonOpenGLServer.h:
+/Users/jonwood/Github_local_dev/cablesStudio/ops/Ops.Extension.Standalone.MacOs.Syphon.SyphonIn/Frameworks/Syphon.framework/Headers/SyphonOpenGLClient.h:
+/Users/jonwood/Github_local_dev/cablesStudio/ops/Ops.Extension.Standalone.MacOs.Syphon.SyphonIn/Frameworks/Syphon.framework/Headers/SyphonOpenGLImage.h:
+/Users/jonwood/Github_local_dev/cablesStudio/ops/Ops.Extension.Standalone.MacOs.Syphon.SyphonIn/Frameworks/Syphon.framework/Headers/SyphonImageBase.h:
+/Users/jonwood/Github_local_dev/cablesStudio/ops/Ops.Extension.Standalone.MacOs.Syphon.SyphonIn/Frameworks/Syphon.framework/Headers/SyphonServer.h:
+/Users/jonwood/Github_local_dev/cablesStudio/ops/Ops.Extension.Standalone.MacOs.Syphon.SyphonIn/Frameworks/Syphon.framework/Headers/SyphonClient.h:
+/Users/jonwood/Github_local_dev/cablesStudio/ops/Ops.Extension.Standalone.MacOs.Syphon.SyphonIn/Frameworks/Syphon.framework/Headers/SyphonImage.h:
